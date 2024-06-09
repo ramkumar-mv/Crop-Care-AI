aa80e5e7959610ebcf666a5035d9928591709c51
Ramkumar M V <86485511+ramkumar-mv@users.noreply.github.com>
2024-06-09 11:02:25 +0530

    Add files via upload

---
+++ App frontend/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdjk_7sXUji_2zi8PecF40S1NS7yLR2Uk",
+    authDomain: "majorproject-60694.firebaseapp.com",
+    projectId: "majorproject-60694",
+    storageBucket: "majorproject-60694.appspot.com",
+    messagingSenderId: "365923730302",
+    appId: "1:365923730302:web:7b9dd38b4d9a68f30de92f",
+    measurementId: "G-WTQT4Q9L73"
+  };
+
+const app1 = initializeApp(firebaseConfig);
+
+export { app1 };
